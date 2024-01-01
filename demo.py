7a34b53ea9b1889bcb298aa5e2415c1bd240200f
Zach English <zenglish555@gmail.com>
2024-01-01 03:39:24 -0800

    Finalized version of the demo

--- demo.py
+++ demo.py
@@ -2,3 +2,18 @@
 x = int(input("Please enter an integer: "))
 
 print("Your input is: " + str(x))
+
+if(x%2==0):
+    print("Your input was even")
+else:
+    print("Your input was odd")
+
+if(x>-10 and x<10):
+    print("You inputted a single-digit number")
+elif(x>-100 and x<100):
+    print("You inputted a double-digit number")
+else:
+    print("Your inputted number had more than two digits")
+
+if(x==8 or x==15):
+    print("You inputted a lucky number!")
